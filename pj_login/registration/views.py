b5ae18e95e64424968ae1df196b1c25c7457f800
Haruki Goto <97876815+H-goto16@users.noreply.github.com>
2022-06-05 11:14:22 +0900

    Update views.py

--- pj_login/registration/views.py
+++ pj_login/registration/views.py
@@ -15,7 +15,5 @@ class ActivateView(TemplateView):
     template_name = "registration/activate.html"
     
     def get(self, request, uidb64, token, *args, **kwargs):
-        # 認証トークンを検証して、
         result = activate_user(uidb64, token)
-        # コンテクストのresultにTrue/Falseの結果を渡します。
         return super().get(request, result=result, **kwargs)
